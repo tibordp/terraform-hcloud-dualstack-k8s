d3adeac3d135b0617a289566640ea005164cf89d
Tibor Djurica Potpara <tibor.djurica@ojdip.net>
2021-06-08 18:28:15 +0100

    fix

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 case "$1" in
-"setup")
+setup)
     terraform apply -auto-approve
     terraform output -raw simple_cluster > simple_cluster.conf
     terraform output -raw ha_cluster > ha_cluster.conf
@@ -11,7 +11,8 @@ case "$1" in
 
     KUBECONFIG=$(pwd)/simple_cluster.conf kubectl wait deployment/nginx --for condition=available
     KUBECONFIG=$(pwd)/ha_cluster.conf kubectl wait deployment/nginx --for condition=available
-"teardown")
+    ;;
+teardown)
     # Try to delete the load-balancers first, as they will not be to deleted otherwise. Do not fail if
     # it fails, otherwise 
     KUBECONFIG=$(pwd)/simple_cluster.conf kubectl delete -f manifest.yaml || true
@@ -19,4 +20,5 @@ case "$1" in
     
     # Disable locking in case `terraform apply` was interrupted
     terraform destroy -auto-approve -lock=false
+    ;;
 esac
